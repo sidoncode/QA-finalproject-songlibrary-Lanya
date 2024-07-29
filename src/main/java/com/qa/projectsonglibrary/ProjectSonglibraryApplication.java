669028e364a8120324235a2e01d4aaca626cdbef
siddharthsharma <siddharth54330@gmail.com>
2024-07-30 00:43:43 +0530

    added swagger added html page changed v2 -> v3 swagger doc

--- src/main/java/com/qa/projectsonglibrary/ProjectSonglibraryApplication.java
+++ src/main/java/com/qa/projectsonglibrary/ProjectSonglibraryApplication.java
@@ -2,8 +2,11 @@ package com.qa.projectsonglibrary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class ProjectSonglibraryApplication {
 
 	public static void main(String[] args) {
